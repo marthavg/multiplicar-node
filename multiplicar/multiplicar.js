f9b14ac74271bee14aa7f2e9a592ae94023a3092
Martha <marthavg.14@gmail.com>
2020-07-04 15:15:48 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,52 @@
+//requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log('================='.green);
+    console.log(`tabla de ${ base }`.green);
+    console.log('================='.green);
+    let data = '';
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${i} = ${base * i}`);
+    }
+
+}
+
+
+
+
+let crearArchivo = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido de la base ${ base } no es un número`);
+            return;
+        }
+        if (!Number(limite)) {
+            reject(`El valor introducido del limite ${ limite } no es un número`);
+            return;
+        }
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${base} * ${i} = ${base * i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${ base}-al-${limite}.txt`, data, (err) => {
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla-${ base}-al-${limite}.txt`)
+        });
+
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
